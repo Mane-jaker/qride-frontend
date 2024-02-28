20bb4e30957a22dc47b44dc6c2baf6c9c91eb139
Mane <angelillo3581@gmail.com>
2024-02-28 15:36:12 -0600

    scroll

--- lib/display/screens/home/home.dart
+++ lib/display/screens/home/home.dart
@@ -10,56 +10,58 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.all(8.0),
-      child: Column(
-        children: [
-          SearchPlace(),
-          SizedBox(
-            height: 15,
-          ),
-          Row(
-            children: [
-              Text(
-                'Rutas cerca de tu ubicación',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          RuteCard(title: 'Ruta 58', subtitle: 'Informacion de la ruta'),
-          RuteCard(title: 'Ruta 58', subtitle: 'Informacion de la ruta'),
-          RuteCard(title: 'Ruta 58', subtitle: 'Informacion de la ruta'),
-          SizedBox(
-            height: 15,
-          ),
-          Row(
-            children: [
-              Text(
-                'Lugares cerca de tu ubicación',
-                style: TextStyle(
-                  fontSize: 18,
-                ),
-                textAlign: TextAlign.left,
-              ),
-            ],
-          ),
-          SizedBox(
-            height: 15,
-          ),
-          SingleChildScrollView(
-            scrollDirection: Axis.horizontal,
-            child: Row(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SearchPlace(),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
               children: [
-                PlaceCard(text: 'Hola'),
-                PlaceCard(text: 'Hola'),
-                PlaceCard(text: 'Hola'),
+                Text(
+                  'Rutas cerca de tu ubicación',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                ),
               ],
             ),
-          )
-        ],
+            SizedBox(
+              height: 15,
+            ),
+            RuteCard(title: 'Ruta 58', subtitle: 'Informacion de la ruta'),
+            RuteCard(title: 'Ruta 58', subtitle: 'Informacion de la ruta'),
+            RuteCard(title: 'Ruta 58', subtitle: 'Informacion de la ruta'),
+            SizedBox(
+              height: 15,
+            ),
+            Row(
+              children: [
+                Text(
+                  'Lugares cerca de tu ubicación',
+                  style: TextStyle(
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 15,
+            ),
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: [
+                  PlaceCard(text: 'Hola'),
+                  PlaceCard(text: 'Hola'),
+                  PlaceCard(text: 'Hola'),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
